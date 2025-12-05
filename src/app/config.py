623101f6568f90aeff41521eda5385df7fdb8038
zsolnai <georg.zsolnai123@gmail.com>
2025-12-05 21:07:34 +0100

    feat(graph): add missing deps and missing settings

--- src/app/config.py
+++ src/app/config.py
@@ -4,9 +4,9 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
-    
+
     neo4j_uri: str = "neo4j+s://localhost:7687"
-    neo4j_user: str = "neo4j"
+    neo4j_username: str = "neo4j"
     neo4j_password: str = ""
     gcp_project_id: str = ""
     pubsub_topic: str = "northern-lights-ingest"
